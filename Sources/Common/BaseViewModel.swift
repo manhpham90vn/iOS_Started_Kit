34f98ab222771d6f52561ed95980c0a8a590a628
Manh Pham <manhpham90vn@icloud.com>
2020-03-15 15:52:08 +0700

    add handle error for rxswift timeout and moya mapping error

--- Sources/Common/BaseViewModel.swift
+++ Sources/Common/BaseViewModel.swift
@@ -5,6 +5,8 @@
 //  Created by Manh Pham on 3/14/20.
 //
 
+import Moya
+
 class BaseViewModel: NSObject { // swiftlint:disable:this final_class
     
     let error = ErrorTracker()
@@ -18,6 +20,20 @@ class BaseViewModel: NSObject { // swiftlint:disable:this final_class
             .subscribe(onNext: { (error) in
                 if let error = error as? AppError {
                     AppHelper.showMessage(title: "Error", message: error.localizedDescription)
+                } else if let error = error as? RxSwift.RxError {
+                    switch error {
+                    case .timeout:
+                        AppHelper.showMessage(title: "Error", message: "Api Timeout")
+                    default:
+                        AppHelper.showMessage(title: "Error", message: "Unknown RxSwift error occurred")
+                    }
+                } else if let error = error as? Moya.MoyaError {
+                    switch error {
+                    case .objectMapping:
+                        AppHelper.showMessage(title: "Error", message: "Map Data Error")
+                    default:
+                        AppHelper.showMessage(title: "Error", message: "Unknown Moya error occurred")
+                    }
                 } else {
                     AppHelper.showMessage(title: "Error", message: error.localizedDescription)
                 }
